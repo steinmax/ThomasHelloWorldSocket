b13db93035b22b80c7d171a8dfa3b8300766d862
steinmax <siegl.maxi@gmail.com>
2021-02-12 22:05:07 +0100

    Initial commit

---
+++ src/main/java/at/steinmax/helloworldsocket/App.java
@@ -0,0 +1,13 @@
+package at.steinmax.helloworldsocket;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/at/steinmax/helloworldsocket/AppTest.java
@@ -0,0 +1,20 @@
+package at.steinmax.helloworldsocket;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
